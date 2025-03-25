3f72408be9e3bcdf7434ea4077677c2c75f60323
lining <lo@qq.com>
2025-03-25 08:53:20 +0800

    使用element-plus

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+//引入element-plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+//全局引入
+app.use(ElementPlus)
+
+app.mount('#app')
